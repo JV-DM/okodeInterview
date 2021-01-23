c44593b9fabc34ac7dff0b6874d094480ffd3a29
JV-DM <javidmwork@outlook.com>
2021-01-23 10:12:23 +0100

    CORS
    Added cors support in the application

--- src/main/java/com/okodeInterview/okode/MovieSearchController.java
+++ src/main/java/com/okodeInterview/okode/MovieSearchController.java
@@ -2,24 +2,21 @@ package com.okodeInterview.okode;
 
 import com.okodeInterview.okode.dataModels.Movie;
 import com.okodeInterview.okode.dataModels.Result;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 @RestController
-@RequestMapping("/movies")
 public class MovieSearchController {
 
     private static String defaultUrl = "https://api.themoviedb.org/3/";
     private static String searchUrl = defaultUrl + "search/movie?api_key=8f27b7eaf57c01db15a6ca4b6b11f9a1&query=";
 
-    @RequestMapping(method = RequestMethod.GET)
-    public Movie[] getMovies(@RequestParam("title") String title) {
+    @CrossOrigin(origins = "http://localhost:8080")
+    @GetMapping("/movies")
+    public Movie[] getMovies(@RequestParam(value = "title", required = true) String title) {
         Movie[] movies = null;
         String requestUrl = "";
         RestTemplate restTemplate = new RestTemplate();
